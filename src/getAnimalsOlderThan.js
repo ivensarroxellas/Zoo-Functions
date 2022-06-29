cd910c2f19b219b17114aa5fe334b419ee24e3bf
Ivens <ivensarroxellas@hotmail.com>
2022-06-29 15:42:01 -0300

    correção req 1

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  return species.find((nameInput) => nameInput.name === animal);
 }
-
+console.log(getAnimalsOlderThan('lion'));
 module.exports = getAnimalsOlderThan;
