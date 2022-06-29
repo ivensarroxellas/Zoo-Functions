b8f60c20b8c1411de720b425b0cd35660b243247
Ivens <ivensarroxellas@hotmail.com>
2022-06-29 18:02:11 -0300

    req 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
+  const employeeFounded = employees.find((employee) =>
+    employee.firstName === employeeName
+     || employee.lastName === employeeName);
+  return employeeFounded;
 }
 
 module.exports = getEmployeeByName;
