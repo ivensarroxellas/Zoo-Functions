8956ca42f978b322c5defe803f213ea83cf4e0e9
Ivens <ivensarroxellas@hotmail.com>
2022-06-29 20:20:43 -0300

    req 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,18 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
   // seu código aqui
+  return employees.some((check) => check.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
+  if (isManager(managerId)) {
+    const objColab = employees.filter((checkEd) => checkEd.managers.includes(managerId));
+    return objColab.map((xablau) => `${xablau.firstName} ${xablau.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
-
 module.exports = { isManager, getRelatedEmployees };
