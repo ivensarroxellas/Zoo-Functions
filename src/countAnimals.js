d5105a0c498a0239f2053f914306afb35373dd81
Ivens <ivensarroxellas@hotmail.com>
2022-06-30 17:55:05 -0300

    req 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
+  const quantity = {};
+  if (!animal) {
+    species.forEach((element) => {
+      quantity[element.name] = element.residents.length;
+    });
+    return quantity;
+  }
+  if (!animal.sex) {
+    const numeroSpecie = species.find((specie) => specie.name === animal.specie);
+    return numeroSpecie.residents.length;
+  }
+  const numeroSpecie = species.find((specie) => specie.name === animal.specie);
+  const numeroSpecieSe = numeroSpecie.residents.filter((sexWished) => sexWished.sex === animal.sex);
+  return numeroSpecieSe.length;
 }
-
+console.log(countAnimals({ specie: 'elephants', sex: 'male' }));
 module.exports = countAnimals;
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,20 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('6 - Implemente os testes da função handlerElephants para obter ao menos 80% de cobertura',
+  it('Verificar se retorna quantidade certa de elefantes', () => {  
+    const expected = [{
+      id: elephantsId,
+      name: 'elephants',
+      popularity: 5,
+      location: 'NW',
+      availability: ['Friday', 'Saturday', 'Sunday', 'Tuesday'],
+      residents: [
+        { name: 'Ilana', sex: 'female', age: 11 },
+        { name: 'Orval', sex: 'male', age: 15 },
+        { name: 'Bea', sex: 'female', age: 12 },
+        { name: 'Jefferson', sex: 'male', age: 4 },
+      ],
+    },];
+
+    expect(getElephants).toBe(expected);
+  }));
