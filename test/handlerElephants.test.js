b219c852a72d14d15313359cb4dd3ee76e2dec1e
Ivens <ivensarroxellas@hotmail.com>
2022-06-30 20:38:45 -0300

    req 7 e 15

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,4 +1,5 @@
 const handlerElephants = require('../src/handlerElephants');
+const getElephants = require('../src/handlerElephants');
 
 describe('6, 7 e 15- Implemente os testes da função handlerElephants para obter ao menos 80%, 90% e 100% de cobertura', () => {
   it('Verifica se caso não forneça parametro a função retorna indefinida', () => {
@@ -13,7 +14,19 @@ describe('6, 7 e 15- Implemente os testes da função handlerElephants para obte
     expect(handlerElephants('names')).toContain('Jefferson');
   });
 
-  it('Verifica se names retorna Jefferson', () => {
+  it('Verifica se averageAge retorna 10.5', () => {
     expect(handlerElephants('averageAge')).toBe(10.5);
+  });
+
+  it('Verifica se botando algo não esperado retorna null', () => {
+    expect(handlerElephants('AAA')).toBe(null);
+  });
+
+  it('Verifica se botando algo não esperado retorna string de invalidez', () => {
+    expect(handlerElephants(10)).toBe('Parâmetro inválido, é necessário uma string');
+  });
+
+  it('Verifica se popularity retorna 5', () => {
+    expect(handlerElephants('popularity')).toBe(5);
   });
 });
